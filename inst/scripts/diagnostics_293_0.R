eada3f6142a31271eead1ee05cbb3dbb3c493fec
Stef van Buuren <stef.vanbuuren@tno.nl>
2024-07-04 22:39:53 +0200

    Update diagnostics to model 20240703/293_0

--- inst/scripts/diagnostics_293_0.R
+++ inst/scripts/diagnostics_293_0.R
@@ -2,14 +2,17 @@
 # Prerequisites: Run the model first
 # Depends on gsed2 lexicon
 # Updated 20221202, update LF item order
+# Updated 20240703, update to dscore 1.9.0
 
 library(dplyr)
 library(dscore)
 library(ggplot2)
+library(gsedread)
 
 # Set and get data and model
 model_name <- "293_0"
 path <- file.path("~/project/gsed/phase1/20221201_remodel", model_name)
+path <- file.path("~/project/gsed/phase1/20240703", model_name)
 data <- readRDS(file.path(path, "data.Rds"))
 model <- readRDS(file.path(path, "model.Rds"))
 
@@ -34,7 +37,8 @@ d_sf <- dscore(data = data,
                items = items_sf,
                key = "gsed",
                xname = "agedays",
-               xunit = "days") |>
+               xunit = "days",
+               verbose = FALSE) |>
   rename(d_sf = d, daz_sf = daz) |>
   select(all_of(c("d_sf", "daz_sf")))
 
@@ -55,7 +59,7 @@ plot1 <- ggplot(joined, aes(x = d_lf, y = d_sf, group = cohort, colour = cohort)
   scale_x_continuous("LF (D-score)", limits = c(10, 80)) +
   scale_y_continuous("SF (D-score)", limits = c(10, 80)) +
   coord_fixed() +
-  geom_abline(slope = 1, colour = "grey", size = 1.5) +
+  geom_abline(slope = 1, colour = "grey", linewidth = 1.5) +
   geom_point(
     size = 0.7,
     shape = 19
@@ -87,8 +91,8 @@ plot2
 plot3 <- ggplot(joined, aes(x = a, y = daz_lf, group = cohort, colour = cohort)) +
   scale_colour_manual(values = col_manual, na.value = "grey") +
   scale_x_continuous("Age (years)", limits = c(0, 3.5)) +
-  scale_y_continuous("LF (DAZ GCDG)", limits = c(-5, 5)) +
-  geom_hline(yintercept = c(-2, 0, 2), colour = "grey", size = 1.5) +
+  scale_y_continuous("LF (DAZ)", limits = c(-5, 5)) +
+  geom_hline(yintercept = c(-2, 0, 2), colour = "grey", linewidth = 1.5) +
   geom_point(
     size = 0.7,
     shape = 19
@@ -100,7 +104,7 @@ plot3
 plot4 <- ggplot(joined, aes(x = a, y = daz_sf, group = cohort, colour = cohort)) +
   scale_colour_manual(values = col_manual, na.value = "grey") +
   scale_x_continuous("Age (years)", limits = c(0, 3.5)) +
-  scale_y_continuous("SF (DAZ GCDG)", limits = c(-5, 5)) +
+  scale_y_continuous("SF (DAZ)", limits = c(-5, 5)) +
   geom_hline(yintercept = c(-2, 0, 2), colour = "grey", size = 1.5) +
   geom_point(
     size = 0.7,
@@ -113,10 +117,10 @@ plot4
 
 plot5 <- ggplot(joined, aes(x = daz_lf, y = daz_sf, group = cohort, colour = cohort)) +
   scale_colour_manual(values = col_manual, na.value = "grey") +
-  scale_x_continuous("LF (DAZ GCDG)", limits = c(-5, 5)) +
-  scale_y_continuous("SF (DAZ GCDG)", limits = c(-5, 5)) +
+  scale_x_continuous("LF (DAZ)", limits = c(-5, 5)) +
+  scale_y_continuous("SF (DAZ)", limits = c(-5, 5)) +
   coord_fixed() +
-  geom_abline(slope = 1, colour = "grey", size = 1.5) +
+  geom_abline(slope = 1, colour = "grey", linewidth = 1.5) +
   geom_point(
     size = 0.7,
     shape = 19
@@ -127,8 +131,8 @@ plot5
 
 plot6 <- ggplot(joined, aes(x = daz_mean, y = daz_diff, group = cohort, colour = cohort)) +
   scale_colour_manual(values = col_manual, na.value = "grey") +
-  scale_x_continuous("(LF + SF) / 2 (DAZ GCDG)", limits = c(-4, 4)) +
-  scale_y_continuous("LF - SF (DAZ GCDG)", limits = c(-4, +4)) +
+  scale_x_continuous("(LF + SF) / 2 (DAZ)", limits = c(-4, 4)) +
+  scale_y_continuous("LF - SF (DAZ)", limits = c(-4, +4)) +
   coord_fixed() +
   geom_hline(yintercept = c(0), colour = "grey", size = 1.5) +
   geom_smooth(se = TRUE, colour = "grey60", size = 1.5) +
@@ -170,14 +174,15 @@ if (!is.null(file) & device == "pdf") {
   dev.off()
 }
 
-# compare difficulty estimates with key gsed2206
-tau_gsed2206 <- get_tau(items = c(items_lf, items_sf), key = "gsed2206")
+# compare difficulty estimates with key gsed1912
+items_gsed <- gsedread::rename_vector(c(items_lf, items_sf), lexin = "gsed2", lexout = "gsed")
+tau_gsed1912 <- get_tau(items = items_gsed, key = "gsed1912")
 tau_294_0 <- get_tau(items = c(items_lf, items_sf), key = "294_0")
 tau_293_0 <- get_tau(items = c(items_lf, items_sf), key = "293_0",
                      itembank = cbind(key = "293_0", model$itembank))
 df <- data.frame(item = c(items_lf, items_sf),
                  instrument = c(rep("LF", length(items_lf)), rep("SF", length(items_sf))),
-                 tau_gsed2206 = tau_gsed2206,
+                 tau_gsed1912 = tau_gsed1912,
                  tau_294_0 = tau_294_0,
                  tau_293_0 = tau_293_0)
 
@@ -189,7 +194,7 @@ g1 <- ggparcoord(df, columns = c(3, 5), groupColumn = 2, scale = 'globalminmax',
   ylab("Item difficulty")
 
 g <- g1 +
-  geom_text(aes(y = tau_gsed2206, x = 1, label = item), data = df[df$instrument == "LF", ],
+  geom_text(aes(y = tau_gsed1912, x = 1, label = item), data = df[df$instrument == "LF", ],
             nudge_x = -0.2, inherit.aes = FALSE, cex = 3, check_overlap = TRUE,
             family = "Courier") +
   geom_text(aes(y = tau_293_0, x = 2, label = item), data = df[df$instrument == "SF", ],
