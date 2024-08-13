1cce8cf7e75d7a28ccb405899a4c0b3f32cf0a0b
Stef van Buuren <stef.vanbuuren@tno.nl>
2024-08-13 13:49:36 +0200

    Update package checking

--- R/update_required_packages.R
+++ R/update_required_packages.R
@@ -3,53 +3,69 @@
 #' This function checks if the required packages are installed and up-to-date.
 #' If not, it asks the user if they want to install the development versions
 #' from GitHub.
+#' @param allow_update Logical. If `TRUE`, the function will install the
+#' development versions from GitHub. The default in `FALSE`
+#' @return Logical. `TRUE` if all required packages are installed and up-to-date.
 #' @export
-update_required_packages <- function() {
+update_required_packages <- function(allow_update = FALSE) {
 
   current <- c("dscore" = "1.9.8",
                "dmetric" = "0.68.1",
                "gseddata" = "1.9.0",
                "gsedread" = "0.8.0",
-               "gsedscripts" = "0.15.0")
+               "gsedscripts" = "0.16.0")
+  OK <- TRUE
 
-  if (!requireNamespace("dscore", quietly = TRUE) &&
-      interactive()) {
-    answer <- askYesNo(paste("Package dscore needed. Install from GitHub?"))
-    if (answer) remotes::install_github("d-score/dscore")
+  if (!requireNamespace("dscore", quietly = TRUE) ||
+      packageVersion("dscore") < current[["dscore"]]) {
+    if (allow_update) {
+      remotes::install_github("d-score/dscore")
+    } else {
+      warning("Package dscore is not installed or not up-to-date. Please install version ", current[["dscore"]])
+      OK <- FALSE
+    }
   }
 
-  if (!requireNamespace("dmetric", quietly = TRUE) &&
-      interactive()) {
-    answer <- askYesNo(paste("Package dmetric needed. Install from GitHub?"))
-    if (answer) remotes::install_github("d-score/dmetric")
+  if (!requireNamespace("dmetric", quietly = TRUE) ||
+      packageVersion("dmetric") < current[["dmetric"]]) {
+    if (allow_update) {
+      remotes::install_github("d-score/dmetric")
+    } else {
+      warning("Package dmetric is not installed or not up-to-date. Please install version ", current[["dmetric"]])
+      OK <- FALSE
+    }
   }
 
-  if (!requireNamespace("gseddata", quietly = TRUE) &&
-      interactive()) {
-    answer <- askYesNo(paste("Package gseddata needed. Install from GitHub?"))
-    if (answer) remotes::install_github("d-score/gseddata")
+  if (!requireNamespace("gseddata", quietly = TRUE) ||
+      packageVersion("gseddata") < current[["gseddata"]]) {
+    if (allow_update) {
+      remotes::install_github("d-score/gseddata")
+    } else {
+      warning("Package gseddata is not installed or not up-to-date. Please install version ", current[["gseddata"]])
+      OK <- FALSE
+    }
   }
 
-  if (!requireNamespace("gsedread", quietly = TRUE) &&
-      interactive()) {
-    answer <- askYesNo(paste("Package gsedread needed. Install from GitHub?"))
-    if (answer) remotes::install_github("d-score/gsedread")
+  if (!requireNamespace("gsedread", quietly = TRUE) ||
+      packageVersion("gsedread") < current[["gsedread"]]) {
+    if (allow_update) {
+      remotes::install_github("d-score/gsedread")
+    } else {
+      warning("Package gsedread is not installed or not up-to-date. Please install version ", current[["gsedread"]])
+      OK <- FALSE
+    }
   }
 
-  if (!requireNamespace("gsedscripts", quietly = TRUE) &&
-      interactive()) {
-    answer <- askYesNo(paste("Package gsedscripts needed. Install from GitHub?"))
-    if (answer) remotes::install_github("d-score/gsedscripts")
+  if (!requireNamespace("gsedscripts", quietly = TRUE) ||
+      packageVersion("gsedscripts") < current[["gsedscripts"]]) {
+    if (allow_update) {
+      remotes::install_github("d-score/gsedscripts")
+    } else {
+      warning("Package gsedscripts is not installed or not up-to-date. Please install version ", current[["gsedscripts"]])
+      OK <- FALSE
+    }
   }
 
-  if (packageVersion("dscore") < current[["dscore"]])
-    warning("Package dscore is not up-to-date. Please update to version ", current[["dscore"]])
-  if (packageVersion("dmetric") < current[["dmetric"]])
-    warning("Package dmetric is not up-to-date. Please update to version ", current[["dmetric"]])
-  if (packageVersion("gseddata") < current[["gseddata"]])
-    warning("Package gseddata is not up-to-date. Please update to version ", current[["gseddata"]])
-  if (packageVersion("gsedread") < current[["gsedread"]])
-    warning("Package gsedread is not up-to-date. Please update to version ", current[["gsedread"]])
-  if (packageVersion("gsedscripts") < current[["gsedscripts"]])
-    warning("Package gsedscripts is not up-to-date. Please update to version ", current[["gsedscripts"]])
+  if (!OK) warning("Use 'update_required_packages(allow_update = TRUE)' to update.")
+  return(OK)
 }
