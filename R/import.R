c09a1b348411485e448f302651014d6f3b262bcd
Stef van Buuren <stef.vanbuuren@tno.nl>
2024-08-14 17:06:12 +0200

    Generalize make_wide() to return 6228 records with measurement instead of one per child (n = 4374)

--- R/import.R
+++ R/import.R
@@ -1,6 +1,7 @@
-#' @importFrom dplyr      all_of any_of across case_match filter
-#'                        group_by
-#'                        mutate rename select slice starts_with ungroup
+#' @importFrom dplyr      all_of any_of across arrange bind_rows
+#'                        case_match filter group_by
+#'                        mutate rename row_number select
+#'                        slice starts_with ungroup
 #' @importFrom dscore     get_itemnames
 #' @importFrom fuzzyjoin  difference_left_join
 #' @importFrom remotes    install_github
--- R/make_wide.R
+++ R/make_wide.R
@@ -7,6 +7,7 @@
 #'                    instruments are `"gpa"`, `"gto"` and `"by3"`.
 #' @param days        Non-negative integer, window width in number of days
 #' @param drop_na_age Logical. Should measurements with missing ages be dropped?
+#' @param include_return  Logical. Should the return visits be included in the join?
 #' @param quiet       Logical. Print messages to terminal?
 #' @return A data frame with one record per child. The first five columns
 #' are administrative variables, followed by the intervals in days per
@@ -20,6 +21,7 @@ make_wide <- function(long,
                       instruments = c("gpa", "gto", "by3"),
                       days = 10L,
                       drop_na_age = TRUE,
+                      include_return = FALSE,
                       quiet = FALSE) {
   stopifnot(days >= 0L)
   stopifnot(length(instruments) >= 2L)
@@ -39,8 +41,8 @@ make_wide <- function(long,
     mutate(
       joinid = .data$subjid * 100,
       across(any_of(items), ~ case_when(
-          .x == "1" ~ 1L,
-          .x == "0" ~ 0L))) |>
+        .x == "1" ~ 1L,
+        .x == "0" ~ 0L))) |>
     select(all_of(adm), any_of(items), "ins", "joinid")
 
   # remove rows with missing agedays
@@ -56,35 +58,34 @@ make_wide <- function(long,
   # split file into three parts: LF, SF and BSID
   sf <- long |>
     filter(.data$ins == "sf") |>
-    select(all_of(adm), any_of(items_sf), "joinid")
+    group_by(.data$subjid) |>
+    mutate(row_number = row_number()) |>
+    select(all_of(adm), any_of(items_sf), "joinid", "row_number")
   lf <- long |>
     filter(.data$ins == "lf") |>
-    select(all_of(adm), any_of(items_lf), "joinid")
+    group_by(.data$subjid) |>
+    mutate(row_number = row_number()) |>
+    select(all_of(adm), any_of(items_lf), "joinid", "row_number")
   bsid <- long |>
     filter(.data$ins == "bsid") |>
-    select(all_of(adm), any_of(items_bsid), "joinid")
+    group_by(.data$subjid) |>
+    mutate(row_number = row_number()) |>
+    select(all_of(adm), any_of(items_bsid), "joinid", "row_number")
 
-  # prevent duplicates, take first record only for each subjid
   sf_first <- sf |>
-    group_by(.data$subjid) |>
-    slice(1L)
-  if (nrow(sf) > nrow(sf_first) && !quiet) {
-    cat("Duplicates measurements removed: ", nrow(sf) - nrow(sf_first), " (gpa)\n")
-  }
+    filter(row_number == 1L)
   lf_first <- lf |>
-    group_by(.data$subjid) |>
-    slice(1L)
-  if (nrow(lf) > nrow(lf_first) && !quiet) {
-    cat("Duplicates measurements removed: ", nrow(lf) - nrow(lf_first), " (gto)\n")
-  }
+    filter(row_number == 1L)
   bsid_first <- bsid |>
-    group_by(.data$subjid) |>
-    slice(1L)
-  if (nrow(bsid) > nrow(bsid_first) && !quiet) {
-    cat("Duplicates measurements removed: ", nrow(bsid) - nrow(bsid_first), " (by3)\n")
-  }
+    filter(row_number == 1L)
+  sf_second <- sf |>
+    filter(row_number >= 2L)
+  lf_second <- lf |>
+    filter(row_number >= 2L)
+  bsid_second <- bsid |>
+    filter(row_number >= 2L)
 
-  #  fuzzy join within days
+  #  fuzzy join within days - FIRST
   phase1 <- sf_first |>
     difference_left_join(lf_first,
                          by = c("joinid", "agedays"),
@@ -101,21 +102,59 @@ make_wide <- function(long,
 
   #  fuzzy join the LF/SF and BSID
   if ("by3" %in% instruments) {
-  phase1 <- phase1 |>
-    difference_left_join(bsid_first,
-                         by = c("joinid", "agedays"),
-                         max_dist = days, distance_col = "dist") |>
-    ungroup() |>
-    rename(cohort = "cohort.x", cohortn = "cohortn.x",
-           subjid = "subjid.x", subjido = "subjido.x",
-           agedays = "agedays.x", joinid = "joinid.x",
-           day_by3 = "agedays.dist") |>
-    select(all_of(adm), any_of(items), "joinid",
-           "day_gpa", "day_gto", "day_by3")
+    phase1 <- phase1 |>
+      difference_left_join(bsid_first,
+                           by = c("joinid", "agedays"),
+                           max_dist = days, distance_col = "dist") |>
+      ungroup() |>
+      rename(cohort = "cohort.x", cohortn = "cohortn.x",
+             subjid = "subjid.x", subjido = "subjido.x",
+             agedays = "agedays.x", joinid = "joinid.x",
+             day_by3 = "agedays.dist") |>
+      select(all_of(adm), any_of(items), "joinid",
+             "day_gpa", "day_gto", "day_by3")
   }
 
+  #  fuzzy join within days - SECOND
+  if (include_return) {
+    #  fuzzy join within days - FIRST
+    sec <- sf_second |>
+      difference_left_join(lf_second,
+                           by = c("joinid", "agedays"),
+                           max_dist = days,
+                           distance_col = "dist") |>
+      ungroup() |>
+      rename(cohort = "cohort.x", cohortn = "cohortn.x",
+             subjid = "subjid.x", subjido = "subjido.x",
+             agedays = "agedays.x", joinid = "joinid.x",
+             day_gto = "agedays.dist") |>
+      mutate(day_gpa = 0) |>
+      select(all_of(adm), any_of(items), "joinid",
+             "day_gpa", "day_gto")
+
+    #  fuzzy join the LF/SF and BSID
+    if ("by3" %in% instruments) {
+      sec <- sec |>
+        difference_left_join(bsid_second,
+                             by = c("joinid", "agedays"),
+                             max_dist = days, distance_col = "dist") |>
+        ungroup() |>
+        rename(cohort = "cohort.x", cohortn = "cohortn.x",
+               subjid = "subjid.x", subjido = "subjido.x",
+               agedays = "agedays.x", joinid = "joinid.x",
+               day_by3 = "agedays.dist") |>
+        select(all_of(adm), any_of(items), "joinid",
+               "day_gpa", "day_gto", "day_by3")
+    }
+  } else {
+    sec <- NULL
+  }
+
+  phase1 <- bind_rows(phase1, sec) |>
+    arrange(.data$subjid, .data$agedays)
+
   phase1 <- phase1 |>
-     select(all_of(adm), any_of(c("day_gpa", "day_gto", "day_by3")), any_of(items))
+    select(all_of(adm), any_of(c("day_gpa", "day_gto", "day_by3")), any_of(items))
 
   return(phase1)
 }
