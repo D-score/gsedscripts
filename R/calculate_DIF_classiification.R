2157cca4dd9896c2824827991cf6711881aef80e
Stef van Buuren <stef.vanbuuren@tno.nl>
2025-07-14 13:29:30 +0200

    Add calculate_DIF_classification() to test DIF by phase and country

---
+++ R/calculate_DIF_classiification.R
@@ -0,0 +1,85 @@
+#' Calculate Differential Item Functioning (DIF) classification table
+#'
+#' Classifies each items according to the Jodoin/Gierl criteria for DIF.
+#' Currently implemented:
+#'
+#'   - phase (1 vs 2)
+#'   - each country vs others
+#'
+#' @param responses A data frame containing the responses to the items.
+#' @return A data frame with items and their classification for DIF.
+#' @export
+calculate_DIF_classification <- function(responses){
+
+  items_sf <- c(get_itemnames(instrument = "gpa", order = "indm"))
+  items_lf <- c(get_itemnames(instrument = "gto"))
+  items <- c(items_sf, items_lf)
+
+  id_cols <- c("cohort", "country", "subjid", "pair")
+  item_var <- "item"
+  response_var <- "response"
+  wide <- responses |>
+    select(all_of(c(id_cols, "agedays", "ins", item_var, response_var))) |>
+    pivot_wider(names_from = all_of(item_var),
+                values_from = all_of(response_var),
+                id_cols = all_of(id_cols)) |>
+    arrange(!!!syms(all_of(id_cols)))
+  agedays_info <- responses |>
+    distinct(!!!syms(c("subjid", "pair", "ins", "agedays"))) |>
+    pivot_wider(names_from = "ins",
+                values_from = "agedays",
+                names_prefix = "agedays_") |>
+    mutate(agedays = .data$agedays_sf)
+  wide <- wide |>
+    left_join(agedays_info, by = c("subjid", "pair")) |>
+    mutate(phase = if_else(.data$country %in% c("BGD", "PAK", "TZA"), 1L, 2L)) |>
+    select(all_of(id_cols), starts_with("agedays"),
+           "phase", any_of(items_sf), any_of(items_lf))
+
+  # DIF analysis: phase (1 vs 2)
+  itemdata <- wide |>
+    select(any_of(items)) |>
+    as.matrix()
+  phase <- wide$phase
+  d <- dscore(wide, xname = "agedays", xunit = "days")$d
+  LR <- genDichoDif(Data = itemdata,
+                    group = phase,
+                    focal.names = 1,
+                    match = d,
+                    method = c("genLogistic"))
+
+  # DIF analysis: each country vs others
+  country <- wide$country
+  LR_list <- vector("list", length(unique(country)))
+  names(LR_list) <- unique(country)
+  for (ct in unique(country)) {
+    LR_list[[ct]] <- genDichoDif(Data = itemdata,
+                                 group = country,
+                                 focal.names = ct,
+                                 match = d,
+                                 method = c("genLogistic"))
+  }
+
+  # Classify according to Jodoin/Gierl criteria
+  DIF_table <- data.frame(
+    item = colnames(itemdata),
+    phase = cut(LR$deltaR2, breaks = c(-Inf, 0.035, 0.07, Inf),
+                labels = c("no", "moderate", "large"), right = FALSE),
+    BGD  = cut(LR_list[["BGD"]]$deltaR2, breaks = c(-Inf, 0.035, 0.07, Inf),
+               labels = c("no", "moderate", "large"), right = FALSE),
+    BRA  = cut(LR_list[["BRA"]]$deltaR2, breaks = c(-Inf, 0.035, 0.07, Inf),
+               labels = c("no", "moderate", "large"), right = FALSE),
+    CHN  = cut(LR_list[["CHN"]]$deltaR2, breaks = c(-Inf, 0.035, 0.07, Inf),
+               labels = c("no", "moderate", "large"), right = FALSE),
+    CIV  = cut(LR_list[["CIV"]]$deltaR2, breaks = c(-Inf, 0.035, 0.07, Inf),
+               labels = c("no", "moderate", "large"), right = FALSE),
+    NLD  = cut(LR_list[["NLD"]]$deltaR2, breaks = c(-Inf, 0.035, 0.07, Inf),
+               labels = c("no", "moderate", "large"), right = FALSE),
+    PAK  = cut(LR_list[["PAK"]]$deltaR2, breaks = c(-Inf, 0.035, 0.07, Inf),
+               labels = c("no", "moderate", "large"), right = FALSE),
+    TZA  = cut(LR_list[["TZA"]]$deltaR2, breaks = c(-Inf, 0.035, 0.07, Inf),
+               labels = c("no", "moderate", "large"), right = FALSE)
+  )
+
+  return(DIF_table)
+}
--- R/import.R
+++ R/import.R
@@ -1,12 +1,14 @@
 #' @importFrom DBI        dbConnect dbDisconnect dbListTables dbReadTable
 #' @importFrom difR       genDichoDif
 #' @importFrom dplyr      all_of any_of across arrange bind_rows
-#'                        case_match filter group_by left_join
+#'                        case_match distinct filter group_by if_else
+#'                        left_join
 #'                        mutate rename row_number select
 #'                        slice starts_with ungroup
-#' @importFrom dscore     get_itemnames
+#' @importFrom dscore     dscore get_itemnames
 #' @importFrom duckdb     duckdb
 #' @importFrom remotes    install_github
-#' @importFrom rlang      .data
+#' @importFrom rlang      .data syms
+#' @importFrom tidyr      pivot_wider
 #' @importFrom utils      askYesNo install.packages packageVersion
 NULL
