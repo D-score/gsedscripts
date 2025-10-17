1cb8458b12033ad48953e5cebc3e68083a94b2ee
Stef van Buuren <stef.vanbuuren@tno.nl>
2025-10-17 17:27:00 +0200

    Update formatting

---
+++ Rmd/anomalies_phase2.txt.R
@@ -0,0 +1,25 @@
+visits |> filter(subjid == "076-GSED-0313")
+    cohort cohortn        subjid agedays vist_type phase       date ins             file parent_id worker_code location caregiver agedays_adj_premature ah01 ah02 ah03 ah04 ah05 ah06 ah07 ah08 m01 m02
+1 GSED-BRA     125 076-GSED-0313     631         1     2 2024-03-21  sf br_sf_2025_06_23   2300533          88        1        11                    NA   NA   NA   NA   NA   NA   NA   NA   NA  NA  NA
+2 GSED-BRA     125 076-GSED-0313      NA         8     2 2024-09-27  lf br_lf_2025_06_23   2300533           5        1        11                    NA   NA   NA   NA   NA   NA   NA   NA   NA  NA  NA
+3 GSED-BRA     125 076-GSED-0313      NA         8     2 2024-09-27  sf br_sf_2025_06_23   2300533          88        1        11                    NA   NA   NA   NA   NA   NA   NA   NA   NA  NA  NA
+
+
+    cohort cohortn        subjid agedays vist_type phase       date ins             file parent_id worker_code location caregiver agedays_adj_premature ah01 ah02 ah03 ah04 ah05 ah06 ah07 ah08 m01 m02
+1 GSED-BRA     125 076-GSED-0668     289         8     2 2025-01-27  lf br_lf_2025_06_23 202390414           2        1        11                    NA    1    0    0    0    0    0    0    0   3   0
+2 GSED-BRA     125 076-GSED-0668     467         1     2 2024-07-23  sf br_sf_2025_06_23 202390414          88        1        11                    NA   NA   NA   NA   NA   NA   NA   NA   NA  NA  NA
+3 GSED-BRA     125 076-GSED-0668     655         1     2 2025-01-27  sf br_sf_2025_06_23 202390414          88        1        11                    NA    1    0    0    0    0    0    0    0   3   0
+
+> visits |> filter(subjid == "076-GSED-0768")
+    cohort cohortn        subjid agedays vist_type phase       date ins             file parent_id worker_code location caregiver agedays_adj_premature ah01 ah02 ah03 ah04 ah05 ah06 ah07 ah08 m01 m02
+1 GSED-BRA     125 076-GSED-0768     310         1     2 2024-08-15  sf br_sf_2025_06_23   2319079          88        1        11                    NA   NA   NA   NA   NA   NA   NA   NA   NA  NA  NA
+2 GSED-BRA     125 076-GSED-0768     485         8     2 2025-02-06  sf br_sf_2025_06_23   2319079          88        4        11                    NA   NA   NA   NA   NA   NA   NA   NA   NA  NA  NA
+
+> visits |> filter(subjid == "076-GSED-0881")
+    cohort cohortn        subjid agedays vist_type phase       date ins             file parent_id worker_code location caregiver agedays_adj_premature ah01 ah02 ah03 ah04 ah05 ah06 ah07 ah08 m01 m02
+1 GSED-BRA     125 076-GSED-0881    1042         1     2 2024-09-10  sf br_sf_2025_06_23 197791104          88        1        11                    NA   NA   NA   NA   NA   NA   NA   NA   NA  NA  NA
+2 GSED-BRA     125 076-GSED-0881    1056         6     2 2024-09-24  sf br_sf_2025_06_23 197791104          88        1        11                    NA   NA   NA   NA   NA   NA   NA   NA   NA  NA  NA
+
+
+
+"076-GSED-0410" "076-GSED-0941" "076-GSED-1683" "528-GSED-0119" "528-GSED-0477" "528-GSED-0519" "528-GSED-0533"
---
+++ inst/scripts/phase2/workfiles/reversals.R
@@ -0,0 +1,11 @@
+
+gpaclc088 Can your child say five or more separate words (e.g., names like 'Mama' or object (KEEP PHASE 1)
+
+gtogmd038 Runs well (KEEP PHASE 2)
+
+gtolgd009 Babbles while playing (REMOVE)
+
+gtofmd005 C5. Discriminates strangers (REMOVE)
+
+gtolgd012 Uses gestures to communicate (REMOVE)
+
