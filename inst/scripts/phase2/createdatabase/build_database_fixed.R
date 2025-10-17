1902b1244f7f2cd3da8d9ca26b898f974b8c1224
Stef van Buuren <stef.vanbuuren@tno.nl>
2025-10-17 17:24:31 +0200

    Update formatting

--- inst/scripts/phase2/createdatabase/build_database_fixed.R
+++ inst/scripts/phase2/createdatabase/build_database_fixed.R
@@ -34,13 +34,18 @@ if (!requireNamespace(pkg, quietly = TRUE) && interactive()) {
   answer <- askYesNo(paste("Package", pkg, "needed. Install from GitHub?"))
   if (answer) remotes::install_github("d-score/gsedread")
 }
+
 require(gsedread, quietly = TRUE, warn.conflicts = FALSE)
-if (packageVersion("gsedread") < "0.23.0") stop("Needs gsedread 0.23.0")
+if (packageVersion("gsedread") < "0.23.0") {
+  stop("Needs gsedread 0.23.0")
+}
 
 # Set paths and filenames
 onedrive <- Sys.getenv("ONEDRIVE_GSED")
-path_phase1 <- file.path("GSED Phase 1 Final Analysis",
-                         "GSED Final Collated Phase 1 Data Files 18_05_22")
+path_phase1 <- file.path(
+  "GSED Phase 1 Final Analysis",
+  "GSED Final Collated Phase 1 Data Files 18_05_22"
+)
 path_phase2 <- "GSED Final Collated Phase 2 Files 02_06_25/Temp Clean LF_ SF_ to add once China data is cleaned" # temporary
 if (!dir.exists(file.path(Sys.getenv("GSED_PHASE2"), "data"))) {
   dir.create(file.path(Sys.getenv("GSED_PHASE2"), "data"), recursive = TRUE)
@@ -48,17 +53,41 @@ if (!dir.exists(file.path(Sys.getenv("GSED_PHASE2"), "data"))) {
 output_fixed <- file.path(Sys.getenv("GSED_PHASE2"), "data/fixed.duckdb")
 
 # Read Phase 1 and Phase 2 data
-phase1 <- read_gsed_fixed(onedrive = onedrive, path = path_phase1, phase = 1, hard_edits = TRUE)
-phase2 <- read_gsed_fixed(onedrive = onedrive, path = path_phase2, phase = 2, hard_edits = TRUE)
+phase1 <- read_gsed_fixed(
+  onedrive = onedrive,
+  path = path_phase1,
+  phase = 1,
+  hard_edits = TRUE
+)
+phase2 <- read_gsed_fixed(
+  onedrive = onedrive,
+  path = path_phase2,
+  phase = 2,
+  hard_edits = TRUE
+)
 
 # Combine Phase 1 and Phase 2 data
 responses <- bind_rows(phase1$responses, phase2$responses)
 visits <- bind_rows(
   phase1$visits |> mutate(phase = 1L),
-  phase2$visits |> mutate(phase = 2L)) |>
-  select(subjid, agedays, vist_type, phase, date, ins, adm,
-         file, parent_id, worker_code, location, caregiver,
-         agedays_adj_premature, everything())
+  phase2$visits |> mutate(phase = 2L)
+) |>
+  select(
+    subjid,
+    agedays,
+    vist_type,
+    phase,
+    date,
+    ins,
+    adm,
+    file,
+    parent_id,
+    worker_code,
+    location,
+    caregiver,
+    agedays_adj_premature,
+    everything()
+  )
 
 # Repair visits and responses
 visits <- repair_visits(visits)
@@ -73,7 +102,7 @@ if (file.exists(output_fixed)) {
 con <- dbConnect(duckdb(), dbdir = output_fixed, read_only = FALSE)
 
 dbWriteTable(con, "responses", responses, overwrite = TRUE)
-dbWriteTable(con, "visits", visits,  overwrite = TRUE)
+dbWriteTable(con, "visits", visits, overwrite = TRUE)
 
 dbDisconnect(con)
 message("Database written to: ", output_fixed)
